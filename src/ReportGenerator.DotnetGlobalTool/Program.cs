287ae9c83f6f91c1b1877745f691b99261825b01
danielpalme <daniel.palme@gmx.de>
2018-05-10 14:51:09 +0200

    Added support for 'DotnetTool'

---
+++ src/ReportGenerator.DotnetGlobalTool/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Palmmedia.ReportGenerator.DotnetGlobalTool
+{
+    /// <summary>
+    /// Command line access to the ReportBuilder.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>Return code indicating success/failure.</returns>
+        internal static int Main(string[] args)
+        {
+            return Core.Program.Main(args);
+        }
+    }
+}
