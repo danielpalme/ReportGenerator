f3db3ff87bb1e08e723f7d6a28f2ba55212ce8c4
danielpalme <daniel.palme@gmx.de>
2019-01-14 22:46:04 +0100

    Fixed line coverage explanation

--- src/ReportGenerator.Core/Reporting/Builders/ReportBuilderBase.cs
+++ src/ReportGenerator.Core/Reporting/Builders/ReportBuilderBase.cs
@@ -64,7 +64,7 @@ namespace Palmmedia.ReportGenerator.Core.Reporting.Builders
             reportRenderer.KeyValueRow(ReportResources.UncoveredLines, (@class.CoverableLines - @class.CoveredLines).ToString(CultureInfo.InvariantCulture));
             reportRenderer.KeyValueRow(ReportResources.CoverableLines, @class.CoverableLines.ToString(CultureInfo.InvariantCulture));
             reportRenderer.KeyValueRow(ReportResources.TotalLines, @class.TotalLines.GetValueOrDefault().ToString(CultureInfo.InvariantCulture));
-            reportRenderer.KeyValueRow(ReportResources.Coverage2, @class.CoverageQuota.HasValue ? $"{@class.CoverageQuota.Value.ToString(CultureInfo.InvariantCulture)}% ({@class.CoveredLines.ToString(CultureInfo.InvariantCulture)} {ReportResources.Of} {@class.TotalLines.GetValueOrDefault().ToString(CultureInfo.InvariantCulture)})" : string.Empty);
+            reportRenderer.KeyValueRow(ReportResources.Coverage2, @class.CoverageQuota.HasValue ? $"{@class.CoverageQuota.Value.ToString(CultureInfo.InvariantCulture)}% ({@class.CoveredLines.ToString(CultureInfo.InvariantCulture)} {ReportResources.Of} {@class.CoverableLines.ToString(CultureInfo.InvariantCulture)})" : string.Empty);
 
             if (@class.CoveredBranches.HasValue && @class.TotalBranches.HasValue)
             {
@@ -187,7 +187,7 @@ namespace Palmmedia.ReportGenerator.Core.Reporting.Builders
             reportRenderer.KeyValueRow(ReportResources.UncoveredLines, (summaryResult.CoverableLines - summaryResult.CoveredLines).ToString(CultureInfo.InvariantCulture));
             reportRenderer.KeyValueRow(ReportResources.CoverableLines, summaryResult.CoverableLines.ToString(CultureInfo.InvariantCulture));
             reportRenderer.KeyValueRow(ReportResources.TotalLines, summaryResult.TotalLines.GetValueOrDefault().ToString(CultureInfo.InvariantCulture));
-            reportRenderer.KeyValueRow(ReportResources.Coverage2, summaryResult.CoverageQuota.HasValue ? $"{summaryResult.CoverageQuota.Value.ToString(CultureInfo.InvariantCulture)}% ({summaryResult.CoveredLines.ToString(CultureInfo.InvariantCulture)} {ReportResources.Of} {summaryResult.TotalLines.GetValueOrDefault().ToString(CultureInfo.InvariantCulture)})" : string.Empty);
+            reportRenderer.KeyValueRow(ReportResources.Coverage2, summaryResult.CoverageQuota.HasValue ? $"{summaryResult.CoverageQuota.Value.ToString(CultureInfo.InvariantCulture)}% ({summaryResult.CoveredLines.ToString(CultureInfo.InvariantCulture)} {ReportResources.Of} {summaryResult.CoverableLines.ToString(CultureInfo.InvariantCulture)})" : string.Empty);
 
             if (summaryResult.CoveredBranches.HasValue && summaryResult.TotalBranches.HasValue)
             {
