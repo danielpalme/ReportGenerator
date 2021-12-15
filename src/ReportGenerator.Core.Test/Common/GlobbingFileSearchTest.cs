0a7a2518d3fa24365144c23a1294bae9c0e95dec
danielpalme <daniel.palme@gmx.de>
2021-12-15 08:07:56 +0100

    Tests

--- src/ReportGenerator.Core.Test/Common/GlobbingFileSearchTest.cs
+++ src/ReportGenerator.Core.Test/Common/GlobbingFileSearchTest.cs
@@ -28,7 +28,7 @@ namespace Palmmedia.ReportGenerator.Core.Test.Common
         public void GetFiles_SingleDirectory_XmlFilesFound()
         {
             var files = GlobbingFileSearch.GetFiles(Path.Combine(FileManager.GetCSharpReportDirectory(), "*.xml")).ToArray();
-            Assert.Equal(21, files.Length);
+            Assert.Equal(22, files.Length);
         }
 
         [Fact]
