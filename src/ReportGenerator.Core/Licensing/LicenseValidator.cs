598a90219c7ec2ab1878e8b4c48fd27e3ff6ff04
Daniel Palme <daniel.palme@gmx.de>
2025-07-20 13:47:13 +0200

    License

--- src/ReportGenerator.Core/Licensing/LicenseValidator.cs
+++ src/ReportGenerator.Core/Licensing/LicenseValidator.cs
@@ -51,6 +51,7 @@ namespace Palmmedia.ReportGenerator.Core.Licensing
             { Guid.Parse("f554292a-d126-402b-a7bf-615b35dc9cd2"), new DateTime(2024, 11, 1) },
             { Guid.Parse("2b1be47b-9785-4c22-a708-b58453e0a669"), new DateTime(2024, 11, 1) },
             { Guid.Parse("c2acc361-1b27-44a9-9e5d-841d8467dac0"), new DateTime(2025, 3, 1) },
+            { Guid.Parse("cd06a2a1-67ff-43b7-baa5-4a54865f2f72"), new DateTime(2025, 8, 1) },
         };
 
         /// <summary>
